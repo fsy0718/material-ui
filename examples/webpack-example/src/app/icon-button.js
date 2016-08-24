7bfa2b5cba12631d250d4d584e39965117e6df26
fanshaoyong <fanshaoyong@huazhu.com>
2016-08-24 17:11:56 +0800

    fixed

--- examples/webpack-example/src/app/icon-button.js
+++ examples/webpack-example/src/app/icon-button.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM, {render} from "react-dom";
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import IconButton from "material-ui/IconButton";
-import {deepOrange500} from 'material-ui/styles/colors';
+import FontIcon from "material-ui/FontIcon";
+import {deepOrange500, yellow500} from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
@@ -17,20 +18,32 @@ class IconButtonExampleSimple extends React.Component{
     console.log(event.target);
   };
   handleBlur(event){
-    console.log(arguments);
+    console.log(arguments, 'blur');
   };
   handleFocus(event){
     console.log(arguments, 'focus');
   }
+  handleTouchTap(event){
+    console.log(arguments, 'touchtap');
+  }
   render(){
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
-        <IconButton iconClassName="muidocs-icon-custom-github" onFocus={this.handleFocus.bind(this)} onBlur={this.handleBlur.bind(this)} onKeyboardFocus={this.handleKeyboardFocus.bind(this)}>
-          click
+      <div>
+        <IconButton 
+          onTouchTap={this.handleTouchTap.bind(this)} 
+          onFocus={this.handleFocus.bind(this)} 
+          onBlur={this.handleBlur.bind(this)} 
+          onKeyboardFocus={this.handleKeyboardFocus.bind(this)}
+        >
+          click me
+          <FontIcon className="material-icons" color={deepOrange500} hoverColor={yellow500}>home</FontIcon>
         </IconButton>
+      </div>
       </MuiThemeProvider>
     )
   }
 }
+
 injectTapEventPlugin();
 render(<IconButtonExampleSimple/>,document.getElementById('app'));
